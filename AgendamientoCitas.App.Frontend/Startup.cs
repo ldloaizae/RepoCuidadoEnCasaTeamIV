364682d13b8bbb7f0d21d0decc8c563e94efb73e
Luis Daniel Loaiza Echeverri <ldloaizae@gmail.com>
2021-10-03 20:26:36 -0500

    Creacion de pagina para entidad prestador de servicios

--- AgendamientoCitas.App.Frontend/Startup.cs
+++ AgendamientoCitas.App.Frontend/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using AgendamientoCitas.App.Persistencia;
+
 namespace AgendamientoCitas.App.Frontend
 {
     public class Startup
@@ -24,6 +26,7 @@ namespace AgendamientoCitas.App.Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IRepositorioPrestadorDeServicio, RepositorioPrestadorDeServicio>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
