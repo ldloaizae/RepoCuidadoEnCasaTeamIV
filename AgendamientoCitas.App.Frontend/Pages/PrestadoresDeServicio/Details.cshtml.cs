23ef3a9142576d4035b7234988fe82e53af0d023
Luis Daniel Loaiza Echeverri <ldloaizae@gmail.com>
2021-10-09 08:41:40 -0500

    Creacion de Pagina detalle de prestador de servicio

--- AgendamientoCitas.App.Frontend/Pages/PrestadoresDeServicio/Details.cshtml.cs
+++ AgendamientoCitas.App.Frontend/Pages/PrestadoresDeServicio/Details.cshtml.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AgendamientoCitas.App.Dominio;
+using AgendamientoCitas.App.Persistencia;
 
 namespace AgendamientoCitas.App.Frontend.Pages.PrestadoresDeServicio
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioPrestadorDeServicio _repoPrestadorDeServicio;
+        public PrestadorDeServicio prestadoreDeServicio {get; set;}
+        public DetailsModel(IRepositorioPrestadorDeServicio repoPrestadorDeServicio)
         {
+            _repoPrestadorDeServicio = repoPrestadorDeServicio;
+        }
+        public IActionResult OnGet(string id)
+        {
+            prestadoreDeServicio = _repoPrestadorDeServicio.GetPrestadorDeServicio(id);
+            if (prestadoreDeServicio == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
