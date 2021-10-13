93725abdc115a5a8c8fedd1fb7e0958cc8fe0dd7
Luis Daniel Loaiza Echeverri <ldloaizae@gmail.com>
2021-10-13 16:59:34 -0500

    Se agrego paginas Edit y Create de doctor y sedes creadas por Juan Naveros

---
+++ AgendamientoCitas.App.Frontend/Pages/Sedes/Edit.cshtml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using AgendamientoCitas.App.Dominio;
+using AgendamientoCitas.App.Persistencia;
+
+
+namespace AgendamientoCitas.App.Frontend.Pages.Sedes
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRepositorioSede _repoSede;
+        public Sede sede {get; set;}
+        public EditModel(IRepositorioSede repoSede)
+        {
+            _repoSede = repoSede;
+        }
+        public IActionResult OnGet(int id)
+        {
+            sede = _repoSede.GetSede(id);
+            if (sede==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
+        }
+        public IActionResult OnPost(Sede sede)
+        {
+            _repoSede.UpdateSede(sede);
+            return RedirectToPage("Index");
+        }
+    }
+}
