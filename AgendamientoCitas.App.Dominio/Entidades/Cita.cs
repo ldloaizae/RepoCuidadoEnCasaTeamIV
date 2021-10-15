1dbce38ef8b0ccb2eba5f9619c41fa31930b4242
Juan Carlos Caicedo R <juancarloscaicedorodriguez@gmail.com>
2021-10-15 14:57:27 -0500

    Revert "Limitaciones de los espacios de las citas"
    This reverts commit eec6b6cbc4da96233b4b6ff34a2e15830f81e207.

--- AgendamientoCitas.App.Dominio/Entidades/Cita.cs
+++ AgendamientoCitas.App.Dominio/Entidades/Cita.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 namespace AgendamientoCitas.App.Dominio
 {
      /// <summary>Class <c>Cita</c>
@@ -9,58 +8,16 @@ namespace AgendamientoCitas.App.Dominio
     {
         // Identificador único de cada persona
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "Elija la fecha de la cita")]
-        [Display(Name = "FechaHora")]
-
         public DateTime FechaHora  { get; set; }
-
         //public string Hora { get; set; }
         //public string Fecha { get; set; }
-        [Required(ErrorMessage = "Elija el tipo de servicio deseado")]
-        [Display(Name = "TipoServicio")]
-
         public TipoServicio TipoServicio { get; set; }
-
-        [Required(ErrorMessage = "Consultorio del Doctor")]
-        [Display(Name = "Consultorio")]
-
         public string Consultorio { get; set; }
-
-        [Required(ErrorMessage = "Establesca la duración de la cita")]
-        [MaxLength(25, ErrorMessage = "Maximo 25 caracteres")]
-        [Display(Name = "Duracion")]
-
         public string Duracion { get; set; }
-
-        [Required(ErrorMessage = "Establesca el valor de la cita")]
-        [MaxLength(25, ErrorMessage = "Maximo 25 caracteres")]
-        [Display(Name = "Valor")]
-
         public string Valor { get; set; }
-
-        [Required(ErrorMessage = "Nombre del paciente")]
-        [MaxLength(50, ErrorMessage = "Maximo 50 caracteres")]
-        [Display(Name = "Paciente")]
-
         public Paciente Paciente { get; set; }
-
-        [Required(ErrorMessage = "Nombre del doctor que atiende la consulta")]
-        [MaxLength(50, ErrorMessage = "Maximo 50 caracteres")]
-        [Display(Name = "Doctor")]
-
         public Doctor Doctor { get; set; }
-
-        [Required(ErrorMessage = "Tipo de atención requerido segun la encuesta")]
-        [MaxLength(50, ErrorMessage = "Maximo 50 caracteres")]
-        [Display(Name = "Encuesta")]
-
         public Encuesta Encuesta { get; set; }
-
-        [Required(ErrorMessage = "Nombre de la sede de atención para la cita")]
-        [MaxLength(50, ErrorMessage = "Maximo 50 caracteres")]
-        [Display(Name = "Sede")]
-
         public Sede Sede { get; set; }
         
     }
