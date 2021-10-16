dd6e4b3af03bbfe4ce8aa54791d33481e423eef8
jnaveros40 <90868042+jnaveros40@users.noreply.github.com>
2021-10-15 20:58:07 -0500

    Add files via upload

--- AgendamientoCitas.App.Dominio/Entidades/Doctor.cs
+++ AgendamientoCitas.App.Dominio/Entidades/Doctor.cs
@@ -1,14 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AgendamientoCitas.App.Dominio
 {
-     /// <summary>Class <c>Doctor</c>
-     /// Modela una Doctor en el sistema 
-     /// </summary>   
+    /// <summary>Class <c>Doctor</c>
+    /// Modela una Doctor en el sistema 
+    /// </summary>   
     public class Doctor:Persona
     {
+
         // Identificador único de cada Doctor
-        public int DoctorId { get; set; }
+        public int IdDoctor { get; set; }
         public string Especialidad { get; set; }
-        public string IdCita { get; set; }
-        
+        [Required(ErrorMessage = "La especialidad del medico es obligatoria")]
+        [Display(Name = "Especialidad")] 
+
+        public string Ddoctor { get; set; }
     }
 }
